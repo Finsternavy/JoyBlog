ac6a73e3ac76db1f49c087a7645e309c848ac96a
Christopher Finster <finsternavy@gmail.com>
2023-02-24 10:49:14 -0600

    initial commit
    initial commit

---
+++ JoyBlog/src/App.vue
@@ -0,0 +1,18 @@
+<template>>
+
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+
+</style>
